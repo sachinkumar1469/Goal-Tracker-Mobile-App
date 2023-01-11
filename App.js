5947f4874b4edb931c64bedeccb3d6dbd8296d72
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2023-01-11 23:32:47 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 
-import { Button, StyleSheet, TextInput } from 'react-native';
+import { Button, ScrollView, StyleSheet, TextInput, FlatList } from 'react-native';
 
 import React, { useState } from 'react';
 import {Text, View} from 'react-native';
 
+import GoalList from './components/GoalList';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
+
+  const [visible,setVisible] = useState(false);
 
   const [text,setText] = useState("");
 
@@ -15,23 +20,37 @@ export default function App() {
   }
 
   function addGoalHandler(){
-    console.log("btn clicked")
+    // console.log("btn clicked")
     setGoals((goal)=>{
-      const newArr = [...goal,text];
+      const newText = {text,id:Math.random().toString()}
+      const newArr = [...goal,newText];
       setText("");
       return newArr;
+    });
+    setVisible(false);
+  }
+
+  function deleteGaol(id){
+    console.log(id);
+    setGoals(goals=>{
+      return goals.filter(goal=>goal.id!==id);
     })
   }
 
 
   return (
     <View style={style.container}>
-      <View style={style.inputContainer}>
-        <TextInput placeholder='Enter Your Goal' style={style.input}  onChangeText={goalInputHandler} value={text}></TextInput>
-        <Button title='Add Goal' onPress={addGoalHandler} />
+      <View style={style.btnView}>
+        <Button title='Add New Goal' onPress={()=>{setVisible(!visible)}}></Button>
       </View>
+      <GoalInput goalInputHandler={goalInputHandler} visible={visible} addGoalHandler={addGoalHandler} text={text}/>
       <View style={style.listContainer}>
-        {goals.map((goal,index)=><Text style={style.goal} key={index}>{goal}</Text>)}
+        <FlatList data={goals} renderItem={(itemData)=>{
+          const item = itemData.item;
+          return <GoalList item={item} deleteGaol={deleteGaol}/>
+          }}
+          keyExtractor={(item,index)=>(index)}
+        />
       </View>
    </View>
   );
@@ -45,36 +64,17 @@ const style = StyleSheet.create({
     // borderColor:"green",
     flex:1
   },
-  inputContainer:{
-    flex:1,
-    flexDirection:'row',
-    borderBottomWidth:1,
-    borderColor:'#cccccc',
-    alignItems:'center',
-    justifyContent:'space-between'
-  },
+  
   listContainer:{
     flex:5,
     paddingTop:20,
     // borderWidth:2
   },
-  input:{
-    borderWidth:1,
-    borderColor:'lightgrey',
-    width:"65%",
-    padding:0,
-    paddingHorizontal:10,
-    height:35
-  },
-  inputBtn:{
-    width:'10%'
-  },
-  goal:{
-    width:'100%',
-    height:35,
-    borderWidth:1,
-    backgroundColor:'blue',
-    borderRadius:5,
+  btnView:{
+    paddingHorizontal:80,
+    paddingVertical:22,
+    borderBottomWidth:1,
+    borderColor:'#cccccc',
   }
 })
 
